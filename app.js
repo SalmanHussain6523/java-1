5f392a0bd4c093ff21b02012afff981485a92240
SalmanHussain6523 <hsalman836@gmail.com>
2024-12-17 14:47:26 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,88 @@
+// chapter no 1 Alert
+
+// question 1
+
+// alert("Hello... Welcome to my Website")
+
+// question 2
+
+// alert("Error! Please enter a valid password.")
+
+// queastion 3
+
+// alert("Welcome to JS land... \n Happy Coding!")
+
+// question 4
+
+// alert("Welcome to JS land...")
+// alert("Happy Coding!")
+
+// question 5
+
+// alert("Hello... I can run JS through my web browser's console")
+
+
+
+
+// Chapter no 2  Variable for strings
+
+// question 1
+
+// var myname= ("Salman M.Hussain")
+// alert(myname)
+
+// var age= ("27 Years Old")
+// alert(age)
+
+// var biodata= ("Certified Mobile Application Development")
+// alert(biodata)
+
+// question 2
+
+// var foodname=("Pizza\n pizz\n piz\n pi\n p")
+// alert(foodname)
+
+// question 3
+
+// var Email= ("My email address is hsalman836@gmail.com")
+// alert(Email)
+
+// question 4
+
+// var Book= ("I am trying to learn from the book A smarter \n way to learn JavaScript")
+// alert(Book)
+
+// question 5
+
+// document .write("JavaScript: the versatile programming language that powers dynamic websites, interactive apps, and modern web experiences.")
+
+// queation 6
+
+// var design= ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// alert(design)
+
+
+// Chapter 3  Variable for numbers 
+
+// queation 1
+
+// var age= ("I am 27 Years Old")
+// alert(age)
+
+//question 2
+
+// var visitor=("You have visited this Website 100 times")
+// alert(visitor)
+
+//question 3
+
+// var birthyear=("My birthday year is 1997 <br> I was born in Karachi...")
+// document.write( birthyear)
+
+//question 4
+
+// var customername=("Babar Azam ordered\n")
+// var quantity=("5\n")
+// var product=("T-shirt(s) on\n")
+// var store=("Salman's Brand")
+// document.write(customername + quantity + product + store)
